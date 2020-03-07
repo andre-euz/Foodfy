34885941281ba271e8f030f73f3438da2339b764
André Euzébio <andre.r.euzebio@gmail.com>
2020-03-07 10:20:47 -0300

    Finishing first part of Foodfy page, now open modal when clicking in the recipes and makes layout reform

--- scripts.js
+++ scripts.js
@@ -1,14 +1,35 @@
 const modalOverlay = document.querySelector('.Modal-overlay');
 const recipes = document.querySelectorAll('.Recipe');
+const recipeModal = document.querySelector('.RecipeModal');
 
 for (let recipe of recipes) {
-    recipe = addEventListener("click",function() {
+    recipe.addEventListener("click",function() {
         modalOverlay.classList.add("active"); 
-        modalOverlay.querySelector("iframe").src = "./modal/modal.html"
+
+        let recipeImg = recipe.querySelector('img').getAttribute('src');        
+        let recipeModalImg = document.createElement('img');
+        recipeModalImg.setAttribute('src',recipeImg);
+        recipeModal.appendChild(recipeModalImg);
+
+        let recipeH3 = recipe.querySelector('h3').textContent;
+        let recipeModalH3 = document.createElement('h3');
+        recipeModalH3.appendChild(document.createTextNode(recipeH3));
+        recipeModal.appendChild(recipeModalH3);
+
+        let recipeP = recipe.querySelector('p').textContent;
+        let recipeModalP = document.createElement('p');
+        recipeModalP.appendChild(document.createTextNode(recipeP));
+        recipeModal.appendChild(recipeModalP);
+ 
+        let closeModal = document.createElement('a');
+        closeModal.classList.add('Close-modal');
+        closeModal.appendChild(document.createTextNode('Fechar modal'));
+        closeModal.addEventListener("click",function() {
+            modalOverlay.classList.remove("active");
+            modalOverlay.querySelector(".Close-modal").href = "";
+        })
+        recipeModal.appendChild(closeModal);
     })
 }
 
-document.querySelector(".Close-modal").addEventListener("click",function() {
-    modalOverlay.classList.remove("active");
-    modalOverlay.querySelector("iframe").src = "";
-})
+
