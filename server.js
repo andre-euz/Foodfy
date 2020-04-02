a10d2fb8abe25cfdbae4e61e19d56ee2e4d5ce4f
André Euzébio <andre.r.euzebio@gmail.com>
2020-04-01 21:41:20 -0300

    Refatorando o Fodfy

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ const nunjucks = require('nunjucks');
 const server = express();
 const recipes = ('./data');
 
+server.use(express.static('public'))
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+});
+
+server.get("/", function(req, res) {
+    return res.render("index", { recipes });
+});
+
+server.listen(5000, function() {
+    console.log("server is running");
+})
